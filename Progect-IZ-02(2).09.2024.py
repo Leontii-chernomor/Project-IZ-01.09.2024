fe5eed6d7a2f110ca181565158b7bfd022b1d355
Leontii-chernomor <Izotovchernomorsk@gmail.com>
2024-09-24 19:44:44 +0300

    математиеское решение второй задачи: ((а/10**4)+b/10**3+c/10**2+d/10+e)*10**4

--- Progect-IZ-02(2).09.2024.py
+++ Progect-IZ-02(2).09.2024.py
@@ -1,4 +1,10 @@
 result_1 = input("Tipe some number")
-result_2 = 1
-result = result_1
-print(result)
+number_1 = int(result_1)//10**4
+number_2 = (int(result_1)%10**4)//10**3
+number_3 = (int(result_1)%10**3)//10**2
+number_4 = (int(result_1)%10**2)//10
+number_5 = (int(result_1)%10**1)//1
+number_6 = (number_5*10**4) + (number_4*10**3) + (number_3*10**2) + (number_2*10) +number_1
+print(number_5,number_4,number_3,number_2,number_1)
+
+print(number_6)
---
+++ Project-IZ-02(1).09.2024..py
@@ -0,0 +1,9 @@
+result_1 = input("Tipe some number")
+number_1 = int(result_1)//1000
+number_2 = (int(result_1) % 1000)//100
+number_3 = (int(result_1) % 100)//10
+number_4 = (int(result_1) % 10)
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_4)
