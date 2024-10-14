05d2b60b61d588cd05c68364f647cbc31bbd030f
Leontii-chernomor <Izotovchernomorsk@gmail.com>
2024-10-14 18:10:36 +0300

    {x for x in range(100) if x % 3 == 0} — генерує множину чисел від 0 до 99, які діляться на 3 без остачі. {x for x in range(100) if x % 5 == 0} — генерує множину чисел від 0 до 99, які діляться на 5 без остачі. multiples_of_3 & multiples_of_5 — знаходить перетин двох множин, тобто числа, які є в обох множинах.

---
+++ Project-IZ-07(4).10.2024.py
@@ -0,0 +1,11 @@
+def common_elements():
+    multiples_of_3 = {x for x in range(100) if x % 3 == 0}
+
+    multiples_of_5 = {x for x in range(100) if x % 5 == 0}
+
+    intersection = multiples_of_3 & multiples_of_5
+
+    return intersection
+
+result = common_elements()
+print(result)
