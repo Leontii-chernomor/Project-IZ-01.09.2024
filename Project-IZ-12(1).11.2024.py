fffc1660e27a22887897a1c8fa2cee580b70dc38
Leontii-chernomor <Izotovchernomorsk@gmail.com>
2024-11-03 21:24:05 +0300

    Читання HTML-файлу: Відкриваємо файл і читаємо його вміст у змінну html. Видалення HTML-тегів: Регулярний вираз r'<[^>]*>' знаходить усі HTML-теги та видаляє їх. Видалення порожніх рядків: Розбиваємо очищений текст на рядки, видаляємо зайві пробіли з кожного рядка і залишаємо лише ті, що не є порожніми. Запис у новий файл: Записуємо результат вказаний файл.

---
+++ Project-IZ-12(1).11.2024.py
@@ -0,0 +1,23 @@
+import codecs
+import re
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+
+
+    cleaned_text = re.sub(r'<[^>]*>', '', html)
+
+
+    cleaned_lines = [line.strip() for line in cleaned_text.splitlines() if line.strip()]
+
+
+    with codecs.open(result_file, 'w', 'utf-8') as output_file:
+        output_file.write("\n".join(cleaned_lines))
+
+    print(f"Файл очищено від HTML-тегів і збережено в '{result_file}'.")
+
+
+
+delete_html_tags( 'draft.html', 'cleaned.txt')
